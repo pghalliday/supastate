b92415a6bd89f73f3e98a59b7c5f1c852daa3663
Peter Halliday <pghalliday@gmail.com>
2025-01-13 17:11:03 +0100

    improve build script

--- tools/scripts/supastate_build.sh
+++ tools/scripts/supastate_build.sh
@@ -1,18 +1,14 @@
 #!/usr/bin/env bash
 
 set -e
-set -E
 
 INPUT_DIR=$1
 DEPS_FILE=$2
 
-CURRENT_DIR=$(pwd)
-TSC_COMMAND="npx tsc --project ""$CURRENT_DIR""/""$INPUT_DIR""/tsconfig.json"
-
 SUPASTATE_SQL_SCRIPT=$INPUT_DIR/sql/supastate.sql
 
 rm -rf "${INPUT_DIR:?}/"lib
-if SUPASTATE_INPUT_FILES=$($TSC_COMMAND --listFiles); then
+if SUPASTATE_INPUT_FILES=$(npx tsc --project "$INPUT_DIR"/tsconfig.json --listFiles); then
   echo "$SUPASTATE_SQL_SCRIPT: $(echo "$SUPASTATE_INPUT_FILES" | sed 's/$/ \\/')" > "$DEPS_FILE"
   cd "$INPUT_DIR"/lib
   node index.js
